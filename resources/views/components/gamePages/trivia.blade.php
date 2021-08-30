960ad51d495fe5be3d44aca4fb306f9e2ac3018d
Tixuga <tixa-f@tutanota.com>
2021-08-30 23:26:53 +0100

    Page of single question finished + answer shuffle

--- resources/views/components/gamePages/trivia.blade.php
+++ resources/views/components/gamePages/trivia.blade.php
@@ -7,9 +7,14 @@
     $wrongAnswerThree = $data['results'][0]['incorrect_answers'][2];
     $category = $data['results'][0]['category'];
     $difficulty = $data['results'][0]['difficulty'];
-    //criar array pras respostas
+    
+    //create array to store and shuffle answers
 
-    //mathRandom no array
+    $answers = array($rightAnswer, $wrongAnswerOne, $wrongAnswerTwo, $wrongAnswerThree);
+
+    shuffle($answers);
+    print_r($answers);
+    print_R($rightAnswer);
 
 @endphp
 <p class="game-info">Category: {{$category}}</p>
@@ -19,25 +24,25 @@
     <div class="form-check">
         <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" >
         <label class="form-check-label" for="exampleRadios1">
-          1.{{$wrongAnswerOne}}
+          1.{{$answers[0]}}
         </label>
       </div>
       <div class="form-check">
         <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios2" value="option2">
         <label class="form-check-label" for="exampleRadios2">
-          2.{{$rightAnswer}}
+          2.{{$answers[1]}}
         </label>
       </div>
       <div class="form-check">
         <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios3" value="option3" >
         <label class="form-check-label" for="exampleRadios3">
-          3.{{$wrongAnswerTwo}}
+          3.{{$answers[2]}}
         </label>
       </div>
       <div class="form-check">
         <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios3" value="option3" >
         <label class="form-check-label" for="exampleRadios3">
-          4.{{$wrongAnswerThree}}
+          4.{{$answers[3]}}
         </label>
       </div>
       <button type="submit" id="confirm-btn" class="btn btn-success" value="submit">CONFIRM ANSWER</button>
--- resources/views/pages/leaderboards.blade.php
+++ resources/views/pages/leaderboards.blade.php
@@ -2,10 +2,10 @@
 
 @section('content')
 
-@component('components.learderboard.leaderTittle')
-@endcomponent
+    @component('components.learderboard.leaderTittle')
+    @endcomponent
 
-@component('components.learderboard.leaderList', ['users'=>$users, 'i'=>0])
-@endcomponent
+    @component('components.learderboard.leaderList', ['users'=>$users, 'i'=>0])
+    @endcomponent
 
 @endsection
